c9fec5e3abc5524086d8147fb78d7fdcf9855b58
Nikola Malesevic <males.nikola@live.com>
2017-12-02 00:53:01 +0100

    test importnig mp3 files

--- src/main/java/controllers/MainScreenController.java
+++ src/main/java/controllers/MainScreenController.java
@@ -1,9 +1,11 @@
 package main.java.controllers;
 
+import com.mpatric.mp3agic.Mp3File;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import main.java.repositories.Mp3Repository;
 import main.java.util.Constants;
 
 import java.io.File;
@@ -22,7 +24,11 @@ public class MainScreenController {
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(Constants.MP3_FILE_TYPE_DESCRIPTION, Constants.MP3_FILE_TYPE_EXTENSION);
         fileChooser.getExtensionFilters().add(extFilter);
         // Open dialog for choosing mp3 files
-        List<File> file = fileChooser.showOpenMultipleDialog(stage);
+        List<File> files = fileChooser.showOpenMultipleDialog(stage);
+        Mp3Repository.getInstance().importFiles(files);
+        for (Mp3File mp3File : Mp3Repository.getInstance().getFiles()) {
+            System.out.println(mp3File.getFilename());
+        }
     }
 
     public void init(Stage stage) {
