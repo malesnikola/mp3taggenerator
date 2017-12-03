365980f0f89854a038bed51d5120d8b021694ddf
Nikola Malesevic <males.nikola@live.com>
2017-12-03 18:28:50 +0100

    fix bug with saving mp3 file with the same name

--- src/main/java/repositories/Mp3Repository.java
+++ src/main/java/repositories/Mp3Repository.java
@@ -1,8 +1,10 @@
 package main.java.repositories;
 
 import com.mpatric.mp3agic.*;
+import main.java.util.Constants;
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -24,8 +26,15 @@ public class Mp3Repository {
         importedFiles.remove(filePath);
     }
 
-    public void saveFile(Mp3File file) throws IOException, NotSupportedException  {
-        file.save(file.getFilename() + "2");
+    public void saveFile(Mp3File file) throws IOException, NotSupportedException {
+        String originalFileName = file.getFilename();
+        String modifiedFileName = originalFileName + ".modified";
+        file.save(modifiedFileName);
+        File oldFile = new File(originalFileName);
+        oldFile.delete();
+        File newlySavedFile = new File(modifiedFileName);
+        boolean success = newlySavedFile.renameTo(new File(originalFileName));
+//        System.out.println(success);
     }
 
     public Map<String, Mp3File> getImportedFiles() {
